0322d34e95160468c4bac402fc05c47631162663
atorresv <aldemart@hotmail.com>
2024-09-05 15:28:00 +0200

    full pack

---
+++ H13_zenden_ontvangen/copytmp.sh
@@ -0,0 +1,7 @@
+cp -R ../__presentation_tmp ./7_sampling
+cp -R ../__presentation_tmp ./6_bitrate
+cp -R ../__presentation_tmp ./5_bandbreedte
+cp -R ../__presentation_tmp ./4_frequentie_modulatie
+cp -R ../__presentation_tmp ./3_amplitude_modulatie
+cp -R ../__presentation_tmp ./2_modulatie
+cp -R ../__presentation_tmp ./1_zenden
